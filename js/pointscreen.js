81277eceacf879fde29c22ab5303a5e44c8bce7b
csu <csu121@bu.edu>
2014-11-12 21:44:54 -0500

    make changes to pointscreen javascript

--- js/pointscreen.js
+++ js/pointscreen.js
@@ -1,5 +1,25 @@
+var humidity = "null";
+var temperature = "null";
+
+$.getJSON( "http://128.197.180.250:5000/sensors/list/", function( data ) {
+  temperature = data.data[0].svalue;
+  humidity = data.data[1].svalue;
+});
+
+
+
 document.getElementById("Humidity").onclick = function() {myFunction()};
 
 function myFunction() {
     document.getElementById("Humidity").innerHTML = "Entered Humidity Screen!";
 }
+
+function displayHum() {
+	document.getElementById("humidty").innerHTML = humidity;
+}
+
+function displayTemp() {
+	document.getElementById("temperature").innerHTML = temperature;
+}
+
+
